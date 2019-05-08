f61efa9d8eba0edc9167fedaa092f56db24603e8
kaizen36 <29834711+kaizen36@users.noreply.github.com>
2019-05-08 22:22:10 +0100

    additional graphs

--- simple_graph/app.py
+++ simple_graph/app.py
@@ -4,9 +4,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/simple-graph")
+def simple_graph():
     return render_template("simple-graph.html")
+
+@app.route("/simple-graph-2")
+def simple_graph_2():
+    return render_template("simple-graph-2.html")
+
+@app.route("/simple-scatter")
+def simple_scatter():
+    return render_template("simple-scatter.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
